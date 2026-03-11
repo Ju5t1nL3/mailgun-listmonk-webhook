c59bacfdaa2f36f9ed9d1cbd312bb7c2407ee682
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-11 00:44:49 -0500

    Verify mailgun's signature on the payload

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+
 from app.models import MailgunPayload
+from app.utils.crypto import verify_mailgun_signature
 
 app = FastAPI()
 
 
 @app.post("/webhook")
 async def receive_webhook(payload: MailgunPayload):
+    payload_signature = payload.signature
+    if not verify_mailgun_signature(
+        payload_signature.timestamp,
+        payload_signature.token,
+        payload_signature.signature,
+    ):
+        raise HTTPException(status_code=401, detail="Invalid Mailgun Signature")
+
     return {"message": "Hello World"}
