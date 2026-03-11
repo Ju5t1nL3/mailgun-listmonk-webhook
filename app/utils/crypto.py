cd59cbd30ee2083a0efd2f1e37f174950bd5f202
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-11 01:11:47 -0500

    Extract environment variable into a config file

--- app/utils/crypto.py
+++ app/utils/crypto.py
@@ -1,13 +1,14 @@
 import hashlib
 import hmac
-import os
 
-mailgun_key = os.getenv("mailgun_signing_key")
+from app.utils.config import settings
 
 
 def verify_mailgun_signature(timestamp: str, token: str, signature: str) -> bool:
     hmac_digest = hmac.new(
-        key=mailgun_key, msg=(timestamp + token).encode(), digestmod=hashlib.sha256
+        key=settings.MAILGUN_SIGNING_KEY,
+        msg=(timestamp + token).encode(),
+        digestmod=hashlib.sha256,
     ).hexdigest()
 
     return hmac.compare_digest(str(signature), str(hmac_digest))
