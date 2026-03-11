aae83c075a5f462d0020ed4f67721cd760d97996
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-11 00:11:34 -0500

    Add function for checking mailgun signatures
    Mailgun signs event requests to ensure authenticity.
    
    https://documentation.mailgun.com/docs/mailgun/user-manual/webhooks/securing-webhooks

---
+++ app/utils/crypto.py
@@ -0,0 +1,13 @@
+import os
+import hmac
+import hashlib
+
+mailgun_key = os.getenv("mailgun_signing_key")
+
+
+def verify_mailgun_signature(timestamp: str, token: str, signature: str) -> bool:
+    hmac_digest = hmac.new(
+        key=mailgun_key, msg=(timestamp + token).encode(), digestmod=hashlib.sha256
+    ).hexdigest()
+
+    return hmac.compare_digest(str(signature), str(hmac_digest))
