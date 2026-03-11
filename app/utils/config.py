1822aec360fed3d3b5748be3575523db8c84ae2b
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-11 02:43:20 -0500

    Add listmonk url and auth as env vars

--- app/utils/config.py
+++ app/utils/config.py
@@ -4,6 +4,10 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Settings(BaseSettings):
     MAILGUN_SIGNING_KEY: str
 
+    LISTMONK_URL: str
+    LISTMONK_API_USER: str
+    LISTMONK_API_TOKEN: str
+
     model_config = SettingsConfigDict(env_file=".env", env_file_encoding="utf-8")
 
 
