7b2d4c6c117b7dc0d69664e9da55aeeb0b2cc983
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-10 23:13:45 -0500

    Add a boilerplate webhook route

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
-def main():
-    print("Hello from mailgun-listmonk-webhook!")
+from fastapi import FastAPI
 
+app = FastAPI()
 
-if __name__ == "__main__":
-    main()
+@app.post("/webhook")
+async def receive_webhook():
+    return {"message": "Hello World"}
