920f621b58c4deb78e2bec9138b4e6ac4bda9a3f
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-11 00:40:22 -0500

    Define mailgun's payload and pass as the body of the POST request

--- app/main.py
+++ app/main.py
@@ -1,8 +1,9 @@
 from fastapi import FastAPI
+from app.models import MailgunPayload
 
 app = FastAPI()
 
 
 @app.post("/webhook")
-async def receive_webhook():
+async def receive_webhook(payload: MailgunPayload):
     return {"message": "Hello World"}
---
+++ app/models.py
@@ -0,0 +1,13 @@
+from pydantic import BaseModel, Field
+from typing import Dict, Any
+
+
+class MailgunSignature(BaseModel):
+    timestamp: str
+    token: str
+    signature: str
+
+
+class MailgunPayload(BaseModel):
+    signature: MailgunSignature
+    event_data: Dict[str, Any] = Field(alias="event-data")
