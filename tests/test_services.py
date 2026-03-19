715000f4d32bb18decae3456f92edfcfc120e30b
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-19 14:42:19 -0500

    Inject mock events as a fixture dependency

--- tests/test_services.py
+++ tests/test_services.py
@@ -10,27 +10,29 @@ from app.schemas import (
 from app.services import forward_bounce
 
 
-def create_test_event(
-    event_type: EventType = EventType.FAILED,
-    severity: EventSeverity = EventSeverity.PERMANENT,
-    delivery_status: DeliveryStatus = DeliveryStatus(
-        message="Failed", description=None
-    ),
-    tags: list[str] = [],
-    campaign_uuid: str = "123-abc",
-) -> EventData:
-    return EventData(
-        event=event_type,
-        recipient="test@tamuhack.org",
-        severity=severity,
-        delivery_status=delivery_status,
-        tags=tags,
-        user_variables=UserVariables(campaign_uuid=campaign_uuid),
-    )
+@pytest.fixture
+def mock_event():
+    def _create(
+        event_type: EventType = EventType.FAILED,
+        severity: EventSeverity = EventSeverity.PERMANENT,
+        delivery_status: DeliveryStatus = DeliveryStatus(
+            message="Failed", description=None
+        ),
+        tags: list[str] = [],
+        campaign_uuid: str = "123-abc",
+    ) -> EventData:
+        return EventData(
+            event=event_type,
+            recipient="test@tamuhack.org",
+            severity=severity,
+            delivery_status=delivery_status,
+            tags=tags,
+            user_variables=UserVariables(campaign_uuid=campaign_uuid),
+        )
 
 
 @pytest.mark.asyncio
-async def test_ignore_irrelevant_events():
-    event = create_test_event(event_type=EventType.ACCEPTED)
+async def test_ignore_irrelevant_events(mock_event):
+    event = mock_event(event_type=EventType.ACCEPTED)
     result = await forward_bounce(event)
     assert result["status"] == "ignored"
