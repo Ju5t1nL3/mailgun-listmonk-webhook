91d6db1178ea73c269367f5d543f13bf7217c2c8
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-23 00:01:40 -0500

    Add docstrings to crypto func

--- app/utils/crypto.py
+++ app/utils/crypto.py
@@ -6,6 +6,13 @@ from app.utils.config import settings
 
 
 def verify_mailgun_signature(timestamp: str, token: str, signature: str) -> bool:
+    """
+    Validates the mailgun signature to ensure request authenticity.
+
+    Checks the HMAC-SHA256 digest against the provided signature to guarantee
+    that the original request came from Mailgun. It also rejects payloads older
+    than 15 minutes.
+    """
     try:
         if abs(time.time() - int(timestamp)) > 900:
             return False
