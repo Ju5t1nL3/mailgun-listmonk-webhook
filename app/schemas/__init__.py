ebc7c0f190191fb5818402849158d9e841d6b231
Ju5t1nL3 <justinle2006@gmail.com>
2026-03-23 01:13:36 -0500

    Explicitly state what this module is exporting

--- app/schemas/__init__.py
+++ app/schemas/__init__.py
@@ -8,3 +8,19 @@ from app.schemas.event import (
 from app.schemas.listmonk import ListmonkMeta, ListmonkPayload, ListmonkSeverity
 from app.schemas.mailgun import MailgunPayload, MailgunSignature
 from app.schemas.webhook import WebhookErrorCode, WebhookResponse, WebhookStatus
+
+__all__ = [
+    "DeliveryStatus",
+    "EventData",
+    "EventSeverity",
+    "EventType",
+    "UserVariables",
+    "ListmonkMeta",
+    "ListmonkPayload",
+    "ListmonkSeverity",
+    "MailgunPayload",
+    "MailgunSignature",
+    "WebhookErrorCode",
+    "WebhookResponse",
+    "WebhookStatus",
+]
